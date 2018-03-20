ee738d59c6d40d386c3e453d0d6dc3ea9142a24d
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-03-20 19:00:05 -0400

    docker

--- files/Test_1.py
+++ files/Test_1.py
@@ -8,7 +8,7 @@ rank = comm.rank
 
 if rank != 0:
 	print "My name is: ", comm.rank
-	os.system("sudo apt-get install docker")
+	os.system("curl -sSL https://get.docker.com | sh")
 	
 '''
 sudo docker swarm leave; sudo docker swarm join --token SWMTKN-1-5sjbfzctsjmf0osxqwijks43xvly4resj3kslh7gs5kxnnu0ci-er6dbk8m3hyngoa4ny194vyd6 192.168.10.1:2377
