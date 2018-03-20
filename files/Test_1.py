6e14206b21e747fe5c28089d12248ea8ce620c71
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-03-20 18:34:06 -0400

    docker

--- files/Test_1.py
+++ files/Test_1.py
@@ -7,5 +7,5 @@ rank = comm.rank
 print "My name is: ", comm.rank
 
 
-os.system("sudo apt-get install docker; curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-")
+os.system("sudo apt-get install docker; curl -sSL https://get.docker.com | sh")
 
