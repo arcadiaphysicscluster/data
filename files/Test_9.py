d4f5e8e00c3dceebed19f0ccb62c78c87efc1789
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-05 18:08:33 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import numpy
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
@@ -7,16 +8,18 @@ name = MPI.Get_processor_name()
 
 if rank == 0:
 	data = [ x for x in range(size * 10)]
-	print 'we will be scattering:', data
+	data_chunks = numpy.array_split(data,size)
+	print 'we will be scattering:', data , 'into', size, 'chunks'
 	
 else:
-	data = None
+	data_chunks = None
 	
-data = comm.scatter(data, root = 0)
-print 'rank', rank, 'has data:', data
-data += 1
+data = comm.scatter(data_chunks, root = 0)
+print 'rank', rank, 'has data:', data_chunks
+for i in range(len(data_chunks)):
+	data_chunks[i] = data_chunks[i] + 1
 
-newData = comm.gather(data, root = 0)
+newData = comm.gather(data_chunks, root = 0)
 
 if rank == 0:
 	print 'master collected:', newData
