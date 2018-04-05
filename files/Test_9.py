64face5ad6b311798a908e1fb56acab25bd09855
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-05 18:33:31 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -1,5 +1,4 @@
 from mpi4py import MPI
-import numpy
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
@@ -8,7 +7,9 @@ name = MPI.Get_processor_name()
 
 if rank == 0:
 	data = [ x for x in range(size * 10)]
-	data_chunks = numpy.array_split(data,size)
+	chunks = [[] for _ in range(size)]
+    for i, chunk in enumerate(data):
+        chunks[i % size].append(chunk)
 	print 'we will be scattering:', data , 'into', size, 'chunks'
 	
 else:
