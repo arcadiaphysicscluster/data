4051e697a2cf7ec35e8493a5e1572d36dbf40351
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-05 20:40:58 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -5,17 +5,17 @@ comm = MPI.COMM_WORLD
 rank = comm.rank
 size = comm.size
 name = MPI.Get_processor_name()
-
+'''
 def split_seq(seq, size):
         newseq = []
         splitsize = 1.0/size*len(seq)
         for i in range(size):
                 newseq.append(seq[int(round(i*splitsize)):int(round((i+1)*splitsize))])
         return newseq
-
+'''
 if rank == 0:
 	data = [ x for x in range(size * 10)]
-	data_chunks = split_seq(data,size)
+#	data_chunks = split_seq(data,size)
 		
 	print 'we will be scattering:', data , 'into', size, 'chunks'
 	
@@ -24,14 +24,14 @@ else:
 	data_chunks = None
 	
 recvbuf = [100]
-data = comm.Scatter(data_chunks, recvbuf, dtype='i')
+comm.Scatter(data, recvbuf, root = 0)
 
-print 'rank', rank, 'has data:', data_chunks
+print 'rank', rank, 'has data:', data
 
 for i in range(len(data_chunks)):
 	data_chunks[i] = data_chunks[i] + 1
 
-newData = comm.Gather(data_chunks, recvbuf, dtype='i')
+comm.Gather(data, recvbuf, root = 0)
 
 if rank == 0:
-	print 'master collected:', newData
+	print 'master collected:', data
