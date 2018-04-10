8dfcfa754b4257d6e9df40e51447da01cde7275a
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-09 21:45:34 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -26,7 +26,7 @@ comm.scatter(data_chunks, root = 0)
 
 print 'rank', rank, 'has data:', data_chunks
 if data_chunks:
-	for i in range(data_chunks.size()):
+	for i in range(len(data_chunks)):
 		data_chunks[i] = data_chunks[i] + 1
 
 comm.gather(data_chunks, root = 0)
