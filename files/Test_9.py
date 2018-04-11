1d0bf47424f5c06c58d18f3255d32bfa84902a6f
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-11 09:51:12 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -14,14 +14,14 @@ if rank == 0:
 	print('we will be scattering:', data , 'into', size, 'chunks')
 	
 recvbuf = np.empty(100, dtype='i')
-comm.Scatter(data_chunks,recvbuf,root = 0)
+data_chunks = comm.Scatter(data_chunks,recvbuf,root = 0)
 
 print(name, 'original data:', data_chunks)
 
 for i in range(len(data_chunks)):
 	data_chunks[i] = data_chunks[i] + 1
 
-comm.Gather(data_chunks,recvbuf,root = 0)
+data_chunks = comm.Gather(data_chunks,recvbuf,root = 0)
 
 if rank == 0:
 	print('master collected:', data_chunks)
