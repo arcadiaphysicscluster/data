681b762b2cb977714dead662eedf098be70286ba
arcadiaphysicscluster <Arcadia.Physics.Cluster@gmail.com>
2018-04-11 10:01:29 -0400

    Bitcoin Test

--- files/Test_9.py
+++ files/Test_9.py
@@ -6,22 +6,22 @@ rank = comm.rank
 size = comm.size
 name = MPI.Get_processor_name()
 
-data_chunks = None
 if rank == 0:
-	data_chunks = np.empty([size, 100], dtype='i')
-	data_chunks.T[:,:] = range(size)
+	data = np.arange(100)
+	data_chunks = np.split(data,size)
 		
-	print('we will be scattering:', data_chunks , 'into', size, 'chunks')
+	print('we will be scattering:', data , 'into', size, 'chunks')
 	
-recvbuf = np.empty(100, dtype='i')
-data_chunks = comm.Scatter(data_chunks,recvbuf,root = 0)
+else:
+	data_chunks = None
 
+data_chunks = comm.scatter(data_chunks, root = 0)
 print(name, 'original data:', data_chunks)
 
 for i in range(len(data_chunks)):
 	data_chunks[i] = data_chunks[i] + 1
 
-data_chunks = comm.Gather(data_chunks,recvbuf,root = 0)
+data_chunks = comm.gather(data_chunks, root = 0)
 
 if rank == 0:
 	print('master collected:', data_chunks)
